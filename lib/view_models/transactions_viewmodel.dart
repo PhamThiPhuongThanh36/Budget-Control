e776a704c474864bb8045ac3c5e12a33bfd40ef7
PhamThiPhuongThanh36 <phamthiphuongthanh36@gmail.com>
2026-01-19 11:07:07 +0700

    feat: add transactions_viewmodel.dart

---
+++ lib/view_models/transactions_viewmodel.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import '../database/daos/transaction_dao.dart';
+import '../repository/database_repository.dart';
+
+class TransactionsViewModel extends ChangeNotifier {
+  final DatabaseRepository _repo;
+  double totalIncome = 0.0;
+  double totalExpense = 0.0;
+  double initialBalance = 0.0;
+
+  double get totalBalance => initialBalance + totalIncome - totalExpense;
+
+  TransactionsViewModel(this._repo) {
+    _repo.totalIncome().listen((value) {
+      totalIncome = value;
+      notifyListeners();
+    });
+    _repo.totalExpense().listen((value) {
+      totalExpense = value;
+      notifyListeners();
+    });
+  }
+
+  void setInitialBalance(double value) {
+    initialBalance = value;
+    notifyListeners();
+  }
+
+  Future<void> addTransaction(int categoryId, double amount, String? note, String type) async {
+    final adjustedAmount = type == 'income' ? amount : -amount;
+    await _repo.addTransaction(categoryId, adjustedAmount, note);
+  }
+
+  Future<List<TransactionWithCategory>> getRecentTransactions() async {
+    final all = await _repo.getAllTransactions();
+    all.sort((a, b) => b.transaction.createdAt.compareTo(a.transaction.createdAt));
+    return all.take(10).toList();
+  }
+}
